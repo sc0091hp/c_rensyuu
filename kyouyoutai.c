2b6c7a634945cb1c6f0eab2b885d98271d84a8dc
sc0091hp <masa0216masa0216masa0216@gmail.com>
2024-09-07 11:56:11 +0900

    kyouyoutai_hennsyuu

--- kyouyoutai.c
+++ kyouyoutai.c
@@ -1,14 +1,12 @@
-#include <stdio.h>
 /******************************************
 https://www.youtube.com/watch?v=AdSgN_Z719Q
 このURLを参考にして演習
 *****************************************/
-
+#include <stdio.h>
 union person{
     char name[16];
     int age;
 };
-
 
 int main(){
     union person Tanaka={"Tanaka"};
