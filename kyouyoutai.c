94752a7a56b790498de7f0b3def8b83d5017385f
sc0091hp <masa0216masa0216masa0216@gmail.com>
2024-09-07 11:15:53 +0900

    kyouyoutai

---
+++ kyouyoutai.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/******************************************
+https://www.youtube.com/watch?v=AdSgN_Z719Q
+このURLを参考にして演習
+*****************************************/
+
+union person{
+    char name[16];
+    int age;
+};
+
+
+int main(){
+    union person Tanaka={"Tanaka"};
+    int ans;
+    ans = sizeof(Tanaka);
+    printf("%d\n", ans);
+    printf("%s\n", Tanaka.name);
+    Tanaka.age = 10000;
+    ans = sizeof(Tanaka);
+    printf("%d\n", ans);
+    printf("%d\n", Tanaka.age);
+}
